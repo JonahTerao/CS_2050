77e2109096c334c0e3b3ef20c881d230290d6dc5
JonahTerao <jterao@msudenver.edu>
2025-01-28 22:00:26 -0700

    Hello World
    Java program printing out "Hello World"

---
+++ CS2050_SPRING2025/EclipseWORKSPACE/CS2050CLASSWORK/JavaClass1.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class JavaClass1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+	}
+
+}
